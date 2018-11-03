0a43ca8fe1814294ed803fce9aacb140e894449c
qwqw12100 <qwqw12100@github.com>
2018-11-03 04:46:05 +0000

    “setup”

--- l6.c
+++ l6.c
@@ -1,24 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
 int main (int argc, char *argv[])
 {
-  FLIE* fd
+  FLIE* fd;
   int n;  
-  fd = fopen("/sys/bus/w1/devices/28-021313ce22aa/w1_slave","r");
-  char buf[sizeof(/sys/bus/w1/devices/28-021313ce22aa/w1_slave)];  
-
-  if(fd == (FILE *)NULL) { 
+  char buf[100];
+  char temp[6];
+  
+  fd = fopen("/sys/bus/w1/devices/28-021313ce22aa/w1_slave","r"); 
+ 
+    if(fd == NULL) { 
     perror("open failed"); 
     (void) exit(1);  
   } 
-
+  
   n = fread(buf, 1, 100, fd); 
-
+  
   if(n == 0) { 
     perror("read fails!"); 
     exit(1); 
   } 
 
-  buf[n] = '\0'; 
-  fprintf(stdout, "Read '%s'\n", buf); 
+  temp[5] = '\0';
+  temp[4] = buf[n-1];
+  temp[3] = buf[n-2];
+  temp[2] = buf[n-3];
+  temp[1] = buf[n-4];
+  temp[0] = buf[n-5];
+  
+  printf("Temperature is '%s' C\n", temp/1000); 
 
   (void)fclose(fd);
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+l6: l6.c 
+	gcc -Wall -ansi -pedantic l6.c -o l6
